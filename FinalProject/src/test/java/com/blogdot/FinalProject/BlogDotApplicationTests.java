f00584fa605564a8eeae223f2e403dac585e893c
Renzo Balbiano <balbianor@gmail.com>
2020-12-13 14:51:19 -0300

    Primer despliegue del esqueleto del proyecto sin trabajar

---
+++ FinalProject/src/test/java/com/blogdot/FinalProject/BlogDotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogdot.FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogDotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
